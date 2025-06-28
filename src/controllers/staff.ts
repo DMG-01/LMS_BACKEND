46d2917adc4059d99562d5398aa01c43b514385d
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2025-06-28 10:14:04 +0100

    staff functionality template

---
+++ src/controllers/staff.ts
@@ -0,0 +1,8 @@
+import express, {Request, Response} from "express"
+
+const uploadResult = async(req:Request, res : Response)=> {}
+
+const editResult = async(req:Request, res:Response)=> {}
+
+const getPatientHistory = async(req:Request, res :Response)=> {}
+
