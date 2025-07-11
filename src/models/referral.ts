80909f38045031a669bb3330fe14c234525e57c4
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2025-07-11 10:30:44 +0100

    referral table initialization

---
+++ src/models/referral.ts
@@ -0,0 +1,49 @@
+import {InferAttributes,Model,  InferCreationAttributes, DataType, CreationOptional, DataTypes} from "sequelize"
+import sequelize from "../connectDb"
+class Referral extends Model<InferAttributes<Referral>, InferCreationAttributes<Referral>>  {
+    declare id : CreationOptional<number>
+    declare name : string
+    declare totalAllocatedDiscount : CreationOptional<number>
+    declare totalDiscountPayed : CreationOptional<number>
+    declare accountNumber : CreationOptional<number>
+    declare bankName : CreationOptional<number>
+}
+
+Referral.init({
+    id : {
+        autoIncrement : true, 
+        type : DataTypes.INTEGER, 
+        primaryKey : true, 
+        allowNull : false
+    }, 
+
+    name : {
+        type : DataTypes.STRING, 
+        allowNull : false
+    }, 
+    totalAllocatedDiscount : {
+        type : DataTypes.FLOAT, 
+        allowNull : false, 
+        defaultValue : 0
+    }, 
+    totalDiscountPayed :  {
+        type : DataTypes.FLOAT, 
+        allowNull : false, 
+        defaultValue : 0
+    }, 
+    accountNumber : {
+        type : DataTypes.INTEGER, 
+        allowNull : true
+    }, 
+    bankName : {
+        type : DataTypes.STRING, 
+        allowNull : false
+    }
+
+
+}, {
+    sequelize, 
+    timestamps : true, 
+    tableName : "referral", 
+    modelName : "referral", 
+})
