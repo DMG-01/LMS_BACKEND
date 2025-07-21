f4ad61a9707956d803fd3ec35615e93b9c7859ab
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2025-07-21 12:20:04 +0100

    added staff authentication to dash route

--- src/routes/dashRoutes.ts
+++ src/routes/dashRoutes.ts
@@ -1,5 +1,6 @@
 import express, {RequestHandler} from "express"
 import {getAllPatient, getAPatientHistory} from "../controllers/dash"
+import staffAuthentication from "../middleware/authentiction";
 const dashRouter = express.Router()
 
 const wrapMiddleware = (fn: Function): RequestHandler => {
@@ -8,8 +9,8 @@ const wrapMiddleware = (fn: Function): RequestHandler => {
   };
 };
 
-dashRouter.get("/dash/patients", wrapMiddleware(getAllPatient))
-dashRouter.get("/dash/patient/:patientId",getAPatientHistory )
+dashRouter.get("/dash/patients",wrapMiddleware(staffAuthentication), wrapMiddleware(getAllPatient))
+dashRouter.get("/dash/patient/:patientId",wrapMiddleware(staffAuthentication), getAPatientHistory )
 
 
 export default dashRouter
