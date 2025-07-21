270c3b5efe1b017a49732a785d4836077955a66c
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2025-07-21 12:38:00 +0100

    added staff authentication to staff route

--- src/routes/staffs.ts
+++ src/routes/staffs.ts
@@ -1,11 +1,18 @@
-import express from "express"
+import express, {RequestHandler} from "express"
 import {uploadResult, editResult, returnAPatientHistory, patientHistory} from "../controllers/staff"
+import staffAuthentication from "../middleware/authentiction";
 
 const staffsRouter = express.Router()
+const wrapMiddleware = (fn: Function): RequestHandler => {
+  return (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
 
-staffsRouter.patch("/staff/upload", uploadResult)
-staffsRouter.patch("/staff/Result/edit",editResult)
-staffsRouter.get("/staff/patientHistory",patientHistory)
-staffsRouter.get("/staff/patientHistory/:patientId",returnAPatientHistory )
+
+staffsRouter.patch("/staff/upload",wrapMiddleware(staffAuthentication), uploadResult)
+staffsRouter.patch("/staff/Result/edit",wrapMiddleware(staffAuthentication),editResult)
+staffsRouter.get("/staff/patientHistory",wrapMiddleware(staffAuthentication),patientHistory)
+staffsRouter.get("/staff/patientHistory/:patientId",wrapMiddleware(staffAuthentication),returnAPatientHistory )
 
 export default staffsRouter
