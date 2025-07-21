bffbd9b79f72c0984462a9c9165bb30894664906
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2025-07-21 12:36:20 +0100

    added staff authentication to service

--- src/routes/service.ts
+++ src/routes/service.ts
@@ -1,6 +1,7 @@
 import express, {RequestHandler} from "express";
 import {createNewServiceTemplate, editProperty, returnAService,getAllServices, removeProperty, changePrice, addNewProperty} from "../controllers/serviceTemplate";
 import { wrap } from "module";
+import staffAuthentication from "../middleware/authentiction";
 
 const serviceRouter = express.Router();
 
@@ -11,13 +12,13 @@ const wrapMiddleware = (fn: Function): RequestHandler => {
 };
 
 
-serviceRouter.post("/service", createNewServiceTemplate);
-serviceRouter.get("/service/:serviceId",returnAService )
-serviceRouter.get("/service", wrapMiddleware(getAllServices))
-serviceRouter.patch("/service/change_price", changePrice)
-serviceRouter.delete("/service/:serviceId/removeproperty/:propertyId", removeProperty)
-serviceRouter.patch("/service/addProperty", addNewProperty)
-serviceRouter.patch('/service/:serviceId/property/:propertyId', editProperty)
+serviceRouter.post("/service",wrapMiddleware(staffAuthentication), createNewServiceTemplate);
+serviceRouter.get("/service/:serviceId",wrapMiddleware(staffAuthentication),returnAService )
+serviceRouter.get("/service", wrapMiddleware(staffAuthentication),wrapMiddleware(getAllServices))
+serviceRouter.patch("/service/change_price", wrapMiddleware(staffAuthentication),changePrice)
+serviceRouter.delete("/service/:serviceId/removeproperty/:propertyId",wrapMiddleware(staffAuthentication), removeProperty)
+serviceRouter.patch("/service/addProperty",wrapMiddleware(staffAuthentication), addNewProperty)
+serviceRouter.patch('/service/:serviceId/property/:propertyId', wrapMiddleware(staffAuthentication),editProperty)
 
 
 
